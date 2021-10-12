15ce4876b81da748270df0947e62095abccbd668
Iman Uddin <87994343+Imanub5@users.noreply.github.com>
2021-10-12 14:11:58 -0500

    Add files via upload

---
+++ User Crud/server.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, redirect, session, request
+
+# import the class from friend.py
+from users import User
+app = Flask(__name__)
+
+
+@app.route("/users")
+def index():
+    # call the get all classmethod to get all friends
+    users = User.get_all()
+    print(users)
+    return render_template("users.html", users = users)
+
+@app.route("/users/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/add_user", methods=['POST'])
+def add():
+
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"]
+    }
+
+    User.add_user(data)
+
+    return redirect("/users")
+
+@app.route("/users/<int:id>")
+def user(id):
+    data = {
+        "id" : id
+    }
+    user = User.one_user(data)
+    return render_template("show.html", user = user)
+
+@app.route("/users/<int:id>/edit/")
+def edit(id):
+    data = {
+        "id" : id
+    }
+
+    user = User.one_user(data)
+    return render_template("edit.html", edituser = user)
+
+@app.route("/edituser/<int:id>", methods=['GET','POST'])
+def edituser(id):
+    print(id)
+    data = {
+        "id" : id,
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"]
+    }
+    
+    User.edit(data)
+    print(data)
+    return redirect("/users/" + str(id))
+
+@app.route("/users/<int:id>/delete/")
+def delete(id):
+    data = {
+        "id" : id
+    }
+
+    user = User.delete(data)
+    return redirect("/users")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
